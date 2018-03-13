0896251c06a8eef9abaf5ebb64021cbda33bb3db
brett-schneider <github@cbret.de>
2018-03-13 23:32:21 +0100

    created

---
+++ BinanceWSApi/BinanceWSApi.h
@@ -0,0 +1,19 @@
+//
+//  BinanceWSApi.h
+//  BinanceWSApi
+//
+//  Created by Brett on 13.03.18.
+//  Copyright © 2018 Christoph Bretschneider. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BinanceWSApi.
+FOUNDATION_EXPORT double BinanceWSApiVersionNumber;
+
+//! Project version string for BinanceWSApi.
+FOUNDATION_EXPORT const unsigned char BinanceWSApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BinanceWSApi/PublicHeader.h>
+
+
